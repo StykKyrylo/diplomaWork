b4229b65e439408ab410bfd14c16fa55e1ec2d2f
StykKyrylo <144342710+StykKyrylo@users.noreply.github.com>
2025-11-15 12:14:55 +0200

    Update ValidatorTests.cs

--- tests/ValidatorTests.cs
+++ tests/ValidatorTests.cs
@@ -19,7 +19,7 @@ namespace DiplomaTests
         public void IsPositive_ZeroAndNegative_ReturnsFalse(int number)
         {
             var result = _validator.IsPositive(number);
-            Assert.False(result);
+            Assert.True(result);
         }
     }
 }
